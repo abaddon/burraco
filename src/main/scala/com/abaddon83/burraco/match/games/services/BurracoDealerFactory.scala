c61e085873e3b650b24eddd7d3fba686dfbfbcac
abaddon <abaddon83@gmail.com>
2020-06-30 22:35:54 +0100

    code formatting

--- src/main/scala/com/abaddon83/burraco/match/games/services/BurracoDealerFactory.scala
+++ src/main/scala/com/abaddon83/burraco/match/games/services/BurracoDealerFactory.scala
@@ -6,13 +6,10 @@ import com.abaddon83.burraco.`match`.games.domainModels.burracoGames.waitingPlay
 import com.abaddon83.burraco.shares.decks.Card
 import com.abaddon83.burraco.shares.players.PlayerIdentity
 
-
-
-
-case class BurracoDealerFactory private(private val burracoDeck: BurracoDeck, private val burracoGame: BurracoGameWaitingPlayers){
+case class BurracoDealerFactory private(private val burracoDeck: BurracoDeck, private val burracoGame: BurracoGameWaitingPlayers) {
   private val numCardsForPlayer = 11
   private val numCardsForDiscardPile = 1
-  private val numCardsPozzetto:Array[Int] = Array(11,18)
+  private val numCardsPozzetto: Array[Int] = Array(11, 18)
 
 
   def dealBurracoCards(): BurracoCardsDealt = {
@@ -25,14 +22,14 @@ case class BurracoDealerFactory private(private val burracoDeck: BurracoDeck, pr
     )
   }
 
-  private def dealCardsToPlayers(): Map[PlayerIdentity,List[Card]] ={
-    burracoGame.listOfPlayers.map(
+  private def dealCardsToPlayers(): Map[PlayerIdentity, List[Card]] = {
+    burracoGame.listOfPlayers().map(
       player => dealCardsToPlayer(player.playerIdentity)
-    ).reduce(_++_)
+    ).reduce(_ ++ _)
   }
 
   private def dealCardsToFirstPozzetto(): MazzettoDeck = {
-    MazzettoDeck.build(grabCards(numCardsPozzetto(burracoGame.listOfPlayers.size%2)))
+    MazzettoDeck.build(grabCards(numCardsPozzetto(burracoGame.listOfPlayers().size % 2)))
   }
 
   private def dealCardsToSecondPozzetto(): MazzettoDeck = {
@@ -43,28 +40,22 @@ case class BurracoDealerFactory private(private val burracoDeck: BurracoDeck, pr
     DiscardPile(grabCards(numCardsForDiscardPile))
   }
 
-  private def dealCardsToPlayer(playerIdentity: PlayerIdentity) : Map[PlayerIdentity,List[Card]] = {
+  private def dealCardsToPlayer(playerIdentity: PlayerIdentity): Map[PlayerIdentity, List[Card]] = {
     Map(playerIdentity -> grabCards(numCardsForPlayer))
   }
 
   private def grabCards(numCards: Int): List[Card] = {
-    (1 to numCards).map(_ =>burracoDeck.grabFirstCard()).toList
+    (1 to numCards).map(_ => burracoDeck.grabFirstCard()).toList
   }
 }
 
-object BurracoDealerFactory{
+object BurracoDealerFactory {
   def apply(burracoGame: BurracoGameWaitingPlayers): BurracoDealerFactory = {
     val deckShuffled = deckShuffle(BurracoDeck())
-    new BurracoDealerFactory(deckShuffled,burracoGame)
+    new BurracoDealerFactory(deckShuffled, burracoGame)
   }
 
-  private def deckShuffle(deck: BurracoDeck): BurracoDeck ={
+  private def deckShuffle(deck: BurracoDeck): BurracoDeck = {
     deck.shuffle()
   }
-
 }
-
-
-
-
-
